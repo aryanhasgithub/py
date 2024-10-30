540730e7e00d809b1634ac5c8221aaeb1dfc1a91
aryanhasgithub <aryansharma3carp@gmail.com>
2024-10-30 16:39:01 +0530

    new

---
+++ new1.py
@@ -0,0 +1,44 @@
+num=4
+name="text"
+booleantype=False
+floattype=34.5
+print("num=",num)
+print("num type is",type(num))
+print("text=",name)
+print("text type is",type(name))
+print("boolean=",booleantype)
+print("booleantype is",type(booleantype))
+print("float=",floattype)
+print("float type is",type(floattype))
+
+print("this is string",str(num))
+print("type:",type(str(num)))
+print("this is int",int(floattype))
+print("type:",type(int(floattype)))
+
+print(num*floattype)
+print(num/floattype)
+print(num-floattype)
+print(num+floattype)
+print(num//floattype)
+print(num%floattype)
+print(num>=floattype)
+print(num<=floattype)
+print(num==floattype)
+print(num!=floattype)
+
+length=len(name)
+print(length)
+print("1st letter is:",name[0])
+print("2nd letter is:",name[1])
+print("4th letter is:",name[3])
+print("sliced string:",name[0:3])
+
+
+
+
+
+
+
+
+
