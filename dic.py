7ed938b9c06ca5ba0266aac0d0ade276341b79ea
aryanhasgithub <aryansharma3carp@gmail.com>
2024-11-20 16:27:42 +0530

    tupp

---
+++ dic.py
@@ -0,0 +1,10 @@
+dic ={'name':'aryan','age':'13','gender':'boy'}
+print(dic)
+dic['age']='14'
+print(dic)
+dic['adress']='noida'
+print(dic)
+print(dic)
+dic.pop('gender')
+print(dic)
+print(dic.get('name'))
--- spiral.py
+++ spiral.py
@@ -3,11 +3,12 @@ turtle.Screen().bgcolor("black")
 turtle.Screen().setup(400,300)
 colors=["Blue","Red","Green","orange"]
 board=turtle.Turtle()
+board.hideturtle()
 while True:
     for x in range (200):
         board.pencolor(colors[x%len(colors)])
         board.width(x/100+1)
         board.forward(x)
         board.left(59)
-        board.right(239)
-turtle.done()
+    board.right(239)
+
