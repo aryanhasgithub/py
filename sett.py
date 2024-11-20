1135b5babae16c10a15b77faa8f67c9d5385d557
aryanhasgithub <aryansharma3carp@gmail.com>
2024-11-20 16:41:58 +0530

    set

---
+++ sett.py
@@ -0,0 +1,12 @@
+set1={1,2,3,3,4,5}
+set2={1,2,3,7}
+print("set1=",set1)
+print("set2=",set2)
+print("adding 9 to set 1")
+set1.add(9)
+print(set1,"added 9 ")
+print("adding 10 to set 2")
+set2.add(10)
+print(set2,"added 9 ")
+print(set1.difference(set2),"differences are given")
+print(set1.symmetric_difference(set2),"all diffrences listed")
